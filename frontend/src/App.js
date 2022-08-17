5edbc5934698e8e28a6b9f4369a900860ee67347
Deepaktenzi <Deepaktenzi@gmail.com>
2022-08-17 10:49:52 +0530

    Files Added

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="#">Home</a>
+      </header>
+      <main>Content List</main>
+    </div>
+  );
+}
+
+export default App;
